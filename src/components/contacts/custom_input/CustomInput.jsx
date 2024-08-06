b6743500879adb68e9c5649d4045d7f04c2d8dcb
karkiyubin <therealyubin@gmail.com>
2024-08-06 20:14:11 +0545

    Complete custom input component

---
+++ src/components/contacts/custom_input/CustomInput.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+import { Input } from "@mantine/core";
+
+const CustomInput = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const crossIcon = (
+    <i
+      className="ri-close-line"
+      style={{ fontSize: "1.3rem", cursor: "pointer", color: "var(--silver)" }}
+      onClick={() => setInputValue("")}
+    />
+  );
+
+  const searchIcon = (
+    <i
+      className="ri-search-2-line"
+      style={{ fontSize: "1.3rem", color: "var(--silver)" }}
+    />
+  );
+
+  return (
+    <div style={{ paddingLeft: "0 1.3rem" }}>
+      <Input
+        size="lg"
+        variant="unstyled"
+        value={inputValue}
+        leftSection={searchIcon}
+        rightSection={crossIcon}
+        placeholder="Search Contacts"
+        rightSectionPointerEvents="all"
+        onChange={(event) => setInputValue(event.currentTarget.value)}
+      />
+    </div>
+  );
+};
+
+export default CustomInput;
