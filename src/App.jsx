de965d7c06cc8264ec2d3d8c13c6f387fd4f4079
karkiyubin <therealyubin@gmail.com>
2024-08-07 22:10:42 +0545

    Simulate server request for data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-import "./App.css";
+import { useEffect, useState } from "react";
 
 import { Contacts } from "./components";
 
+import "./App.css";
+
 function App() {
+  const [userList, setUserList] = useState(null);
+
+  useEffect(() => {
+    const getUserList = async () => {
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+
+        const result = await fetch("./userdata.json");
+
+        if (result.status === 200) {
+          const jsonData = await result.json();
+          setUserList(jsonData);
+        }
+      } catch (e) {
+        console.error("Error getting user list:", e);
+      }
+    };
+
+    getUserList();
+  }, []);
+
   return (
     <div className="home-container">
-      <Contacts />
+      <Contacts userList={userList?.data || []} />
     </div>
   );
 }
